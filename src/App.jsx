ef34cc93661e9a84345c527065fa8ebe3c62c9d9
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-06-29 22:40:11 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar';
 import Cover from './components/Cover'
+import Hero from './components/Hero'
+import Footer1 from './components/Footer1'
+import Work from './components/Work'
+import Companies from './components/Companies'
 
 
 const App = () => {
@@ -9,7 +13,11 @@ const App = () => {
     
     <div>
     <Navbar/>
+    <Hero />
     <Cover />
+    <Work />
+    <Companies />
+    <Footer1 />
     </div>
   )
 }
